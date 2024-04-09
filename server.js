ef0f003b1e1185109175f06c9aa0ff592bf756f9
Sufyan <Sufyan265@users.noreply.github.com>
2024-04-10 02:42:26 +0500

    33

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PDFMerger = require('pdf-merger-js');
 // const schedule = require('node-schedule');
 
 // Updated upload destination to use /tmp directory, which is writable in Vercel environment
-const upload = multer({ dest: 'tmp/uploads' });
+const upload = multer({ dest: '/tmp/uploads' });
 const app = express();
 const port = process.env.PORT || 3000;
 

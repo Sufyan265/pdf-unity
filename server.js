d3cf3b13680ea6924ee5efac45f19f76efedaad1
Sufyan <Sufyan265@users.noreply.github.com>
2024-04-10 16:07:36 +0500

    777

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
 const express = require('express');
 const path = require('path');
-const multer = require('multer');
-const bodyParser = require('body-parser');
 const { PDFDocument } = require('pdf-lib');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(fileUpload()); // Use express-fileupload middleware
 
 const websitePath = path.join(__dirname, '/src');
 app.use(express.static(websitePath));
-
-// Set up multer for handling file uploads
-const upload = multer({
-  storage: multer.memoryStorage(), // Store files in memory
-  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
-});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/merge', upload.array('pdfs', 500), async (req, res) => {
+app.post('/merge', async (req, res) => {
   try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+
     const mergedPdf = await PDFDocument.create();
 
     // Loop through each uploaded file
-    for (const file of req.files) {
-      const pdfBytes = file.buffer;
+    for (const file of Object.values(req.files.pdfs)) {
+      const pdfBytes = file.data; // Access file data
       const pdfDoc = await PDFDocument.load(pdfBytes);
       const pages = await mergedPdf.copyPages(pdfDoc, pdfDoc.getPageIndices());
       pages.forEach((page) => mergedPdf.addPage(page));

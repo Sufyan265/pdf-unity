67c07e54a17959c3513557d6acc0c00fdf54dd65
Sufyan <Sufyan265@users.noreply.github.com>
2024-04-10 15:15:54 +0500

    6666

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 const { PDFDocument } = require('pdf-lib');
 
-const upload = multer(); // Remove the destination option, as we won't be saving files locally
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -14,14 +13,21 @@ app.use(bodyParser.json());
 const websitePath = path.join(__dirname, '/src');
 app.use(express.static(websitePath));
 
+// Set up multer for handling file uploads
+const upload = multer({
+  storage: multer.memoryStorage(), // Store files in memory
+  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
+});
+
 app.get('/', (req, res) => {
-  res.send(websitePath);
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/merge', upload.array('pdfs', 500), async (req, res) => {
   try {
     const mergedPdf = await PDFDocument.create();
-    
+
+    // Loop through each uploaded file
     for (const file of req.files) {
       const pdfBytes = file.buffer;
       const pdfDoc = await PDFDocument.load(pdfBytes);
@@ -29,8 +35,9 @@ app.post('/merge', upload.array('pdfs', 500), async (req, res) => {
       pages.forEach((page) => mergedPdf.addPage(page));
     }
 
+    // Serialize the merged PDF to bytes
     const mergedPdfBytes = await mergedPdf.save();
-    
+
     // Set headers to indicate that the response is a PDF file
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=merged.pdf');
